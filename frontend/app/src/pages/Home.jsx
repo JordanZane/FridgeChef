102c729898a07a115651d0a760f8034304e83106
Zanetti Jordan <jzanetti1@outlook.fr>
2024-05-14 18:37:28 +0200

    Update Home component & styles

--- frontend/app/src/pages/Home.jsx
+++ frontend/app/src/pages/Home.jsx
@@ -1,10 +1,29 @@
 import React from 'react';
+import BannerHome from '../assets/banner-home.jpg';
 
 const Home = () => {
   return (
-    <div className="home">
-      <h1>Accueil</h1>
-    </div>
+    <main className="home">
+      <div className="banner-container">
+        <div className="container">
+          <h1>FridgeChef</h1>
+          <div className="search-ingredients-input">
+            <input
+              type="text"
+              id="ingredients"
+              name="ingredients"
+              placeholder="Add ingredients to find recipes (flour, beef, bread, eggs...)"
+            />
+            <button type="submit">Add</button>
+          </div>
+        </div>
+      </div>
+      <div className="recipes-find-container">
+        <div className="container">
+          <h2>Recipes</h2>
+        </div>
+      </div>
+    </main>
   );
 };
 
