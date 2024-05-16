e13ce79c32fdc055397a09c20ac08aff2987ca72
Zanetti Jordan <jzanetti1@outlook.fr>
2024-05-16 16:56:27 +0200

    Added functionality for displaying ingredient suggestions, managing ingredient list, and applied styling

--- frontend/app/src/pages/Home.jsx
+++ frontend/app/src/pages/Home.jsx
@@ -1,7 +1,50 @@
-import React from 'react';
-import BannerHome from '../assets/banner-home.jpg';
+import React, { useState } from 'react';
 
 const Home = () => {
+  const [inputText, setInputText] = useState('');
+  const [suggestions, setSuggestions] = useState([]);
+  const [ingredientsSearch, setIngredientsSearch] = useState([]);
+
+  const ApiKey = process.env.REACT_APP_API_KEY;
+
+  const handleInputChange = (e) => {
+    const userText = e.target.value;
+    setInputText(userText);
+
+    fetch(
+      `https://api.spoonacular.com/food/ingredients/autocomplete?query=${userText}&number=5&apiKey=${ApiKey}`
+    )
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Error fetching ingredient suggestions');
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setSuggestions(data);
+      })
+      .catch((error) => {
+        console.error('Error fetching ingredient suggestions:', error);
+      });
+  };
+
+  const handleAddIngredients = (e) => {
+    if (inputText && !ingredientsSearch.includes(inputText)) {
+      setIngredientsSearch([...ingredientsSearch, inputText]);
+      setInputText('');
+    }
+  };
+
+  const handleRemoveIngredients = (index) => {
+    const newIngredients = ingredientsSearch.filter((_, i) => i !== index);
+    setIngredientsSearch(newIngredients);
+  };
+
+  const handleSuggestionsClick = (Suggestiontext) => {
+    setInputText(Suggestiontext);
+    setSuggestions([]);
+  };
+
   return (
     <main className="home">
       <div className="banner-container">
@@ -13,8 +56,46 @@ const Home = () => {
               id="ingredients"
               name="ingredients"
               placeholder="Add ingredients to find recipes (flour, beef, bread, eggs...)"
+              value={inputText}
+              onChange={handleInputChange}
             />
-            <button type="submit">Add</button>
+            <button type="submit" onClick={handleAddIngredients}>
+              Add
+            </button>
+            {suggestions.length > 0 && (
+              <div className="suggestions-container">
+                <ul>
+                  {suggestions.map((suggestion, index) => (
+                    <li key={index}>
+                      <button
+                        className="suggestion"
+                        onClick={(e) => handleSuggestionsClick(suggestion.name)}
+                      >
+                        {suggestion.name}
+                      </button>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            )}
+          </div>
+
+          <div className="search-ingredients-container">
+            <ul>
+              {ingredientsSearch.map((ingredient, index) => (
+                <li key={index}>
+                  {ingredient}
+                  <div className="remove-btn-container">
+                    <button
+                      onClick={() => handleRemoveIngredients(index)}
+                      className="remove-btn"
+                    >
+                      x
+                    </button>
+                  </div>
+                </li>
+              ))}
+            </ul>
           </div>
         </div>
       </div>
